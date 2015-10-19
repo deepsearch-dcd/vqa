47541dc540019a147481f9a9cb82782657f90f99
yourtone <yuetanlin@gmail.com>
2015-10-19 19:00:02 +0800

    updata all rnn modules, add test programs

--- module/RNN.lua
+++ module/RNN.lua
@@ -1,16 +1,51 @@
 -- RNN modified from http://apaszke.github.io/assets/posts/lstm-explained/LSTM.lua
 -- ref: char-rnn
 --[[
-  local RNN = require 'modules/RNN.lua'
-  rnnmodule = RNN.create(input_size, rnn_size, num_layer[, dropout])
-  (num_layer+1) inputs, num_layer outputs
+  RNN = require 'modules/RNN'
+
+  -- single rnn has 2 inputs, 1 outputs
+  singlernn = RNN.single(input_size, rnn_size)
+  inputs = {torch.rand(input_size), torch.rand(rnn_size)}
+  outputs = singlernn:forward(inputs)
+
+  -- multiple rnn has (num_layer+1) inputs, (num_layer) outputs
+  rnn_sizes = {rnn_size1, rnn_size2}
+  multiplernn = RNN.multiple(input_size, rnn_sizes, num_layer[, dropout])
+  inputs = {torch.rand(input_size), torch.rand(rnn_size1), torch.rand(rnn_size2)}
+  outputs = multiplernn:forward(inputs)
+
+  -- general rnn
+  rnnmodule1 = RNN.create(input_size, rnn_size)
+  rnnmodule2 = RNN.create(input_size, rnn_size, num_layer[, dropout])
+  rnnmodule3 = RNN.create(input_size, rnn_sizes, num_layer[, dropout])
 --]]
 require 'nn'
 require 'nngraph'
 
 local RNN = {}
 
-function RNN.create(input_size, rnn_size, num_layer, dropout)
+function RNN.single(input_size, rnn_size)
+  --------------------- input structure ---------------------
+  local inputs = {}
+  table.insert(inputs, nn.Identity()())   -- network input
+  table.insert(inputs, nn.Identity()())   -- h at time t-1
+  local input = inputs[1]
+  local prev_h = inputs[2]
+
+  -------------------- next hidden state --------------------
+  local i2h = nn.Linear(input_size, rnn_size)(input) -- input to hidden
+  local h2h = nn.Linear(rnn_size, rnn_size)(prev_h)  -- hidden to hidden
+  local next_h = nn.CAddTable()({i2h, h2h})          -- i2h + h2h
+
+  --------------------- output structure --------------------
+  local outputs = {}
+  table.insert(outputs, next_h)
+
+  -- packs the graph into a convenient module with standard API (:forward(), :backward())
+  return nn.gModule(inputs, outputs)
+end
+
+function RNN.multiple(input_size, rnn_sizes, num_layer, dropout)
   dropout = dropout or 0
   --------------------- input structure ---------------------
   local inputs = {}
@@ -19,30 +54,47 @@ function RNN.create(input_size, rnn_size, num_layer, dropout)
     table.insert(inputs, nn.Identity()())   -- h at time t-1
   end
 
-  local input_L, input_size_L
+  local inputs_L = {}, input_size_L
   local outputs = {}
+  -- for layer 1
+  table.insert(inputs_L, inputs[1])
+  input_size_L = input_size
   for L = 1,num_layer do
-    if L == 1 then
-      input_L = inputs[1]
-      input_size_L = input_size
-    else
-      input_L = outputs[(L-1)]
-      if dropout > 0 then input_L = nn.Dropout(dropout)(input_L) end
-      input_size_L = rnn_size
-    end
-    local prev_h = inputs[L+1]
+    table.insert(inputs_L, inputs[L+1]) -- prev_h
+    local rnn_size_L = rnn_sizes[L]
 
-    -------------------- next hidden state --------------------
-    local i2h = nn.Linear(input_size_L, rnn_size)(input_L) -- input to hidden
-    local h2h = nn.Linear(rnn_size, rnn_size)(prev_h)      -- hidden to hidden
-    local next_h = nn.CAddTable()({i2h, h2h})              -- i2h + h2h
+    local outputs_L = RNN.single(input_size_L, rnn_size_L)(inputs_L)
+    local next_h = (outputs_L)
 
     --------------------- output structure --------------------
     table.insert(outputs, next_h)
+
+    -- for next layer
+    inputs_L = {}
+    local next_input = next_h
+    if dropout > 0 then next_input = nn.Dropout(dropout)(next_input) end
+    table.insert(inputs_L, next_input) -- next_h
+    input_size_L = rnn_size_L
   end
 
   -- packs the graph into a convenient module with standard API (:forward(), :backward())
   return nn.gModule(inputs, outputs)
+end
+
+function RNN.create(input_size, rnn_size, num_layer, dropout)
+  if type(rnn_size) == 'number' then -- rnn_size contain a value
+    if num_layer then -- num_layer is not nil (multiple layers with equal rnn size)
+      assert(num_layer > 0, 'num_layer MUST be GREATER THAN 0!')
+      local rnn_sizes = {}
+      for i=1,num_layer do table.insert(rnn_sizes, rnn_size) end
+      return RNN.multiple(input_size, rnn_sizes, num_layer, dropout)
+    else -- num_layer is nil (one layer rnn)
+      return RNN.single(input_size, rnn_size)
+    end
+  else -- rnn_size contain a table of values (multiple layers with different rnn size)
+    assert(#rnn_size == num_layer, 'number of rnn_sizes MUST EQUAL num_layer!')
+    return RNN.multiple(input_size, rnn_size, num_layer, dropout)
+  end
 end
 
 return RNN
--- test/test_gru_forward.lua
+++ test/test_gru_forward.lua
@@ -1,6 +1,6 @@
 require 'nn'
 require 'nngraph'
-local LSTM = require 'module/LSTM'
+local GRU = require 'module/GRU'
 
 local inputs
 local outputs
@@ -15,45 +15,39 @@ local num_layer = #rnn_sizes
 local dropout = 0.5
 
 local input = torch.rand(input_size)
-local prev_c = torch.rand(rnn_size)
 local prev_h = torch.rand(rnn_size)
-local prev_c1 = torch.rand(rnn_size1)
 local prev_h1 = torch.rand(rnn_size1)
-local prev_c2 = torch.rand(rnn_size2)
 local prev_h2 = torch.rand(rnn_size2)
-local prev_c3 = torch.rand(rnn_size3)
 local prev_h3 = torch.rand(rnn_size3)
 
 -- single rnn
-local singlernn = LSTM.single(input_size, rnn_size)
-inputs = {input, prev_c, prev_h}
+local singlernn = GRU.single(input_size, rnn_size)
+inputs = {input, prev_h}
 outputs = singlernn:forward(inputs)
-local next_c = outputs[1]
-local next_h = outputs[2]
+local next_h = outputs
 print('input size: ' .. input:size()[1])
 print('rnn size: ' .. prev_h:size()[1])
-print('next_c size: ' .. next_c:size()[1])
 print('next_h size: ' .. next_h:size()[1])
 print('single outputs:', outputs)
 
 -- multiple rnn
-local multiplernn = LSTM.multiple(input_size, rnn_sizes, num_layer, dropout)
-inputs = {input, prev_c1, prev_h1, prev_c2, prev_h2, prev_c3, prev_h3}
+local multiplernn = GRU.multiple(input_size, rnn_sizes, num_layer, dropout)
+inputs = {input, prev_h1, prev_h2, prev_h3}
 outputs = multiplernn:forward(inputs)
 print('multiple outputs:', outputs)
 
 -- general rnn modules
-local rnnmodule1 = LSTM.create(input_size, rnn_size)
-inputs = {input, prev_c, prev_h}
+local rnnmodule1 = GRU.create(input_size, rnn_size)
+inputs = {input, prev_h}
 outputs = rnnmodule1:forward(inputs)
 print('general single outputs:', outputs)
-local rnnmodule2 = LSTM.create(input_size, rnn_size, num_layer, dropout)
-inputs = {input, prev_c, prev_h, prev_c, prev_h, prev_c, prev_h}
+local rnnmodule2 = GRU.create(input_size, rnn_size, num_layer, dropout)
+inputs = {input, prev_h, prev_h, prev_h}
 outputs = rnnmodule2:forward(inputs)
 print('general multiple same sized rnn outputs:', outputs)
-local rnnmodule3 = LSTM.create(input_size, rnn_sizes, num_layer, dropout)
-local rnnmodule4 = LSTM.create(input_size, rnn_sizes, num_layer)
-inputs = {input, prev_c1, prev_h1, prev_c2, prev_h2, prev_c3, prev_h3}
+local rnnmodule3 = GRU.create(input_size, rnn_sizes, num_layer, dropout)
+local rnnmodule4 = GRU.create(input_size, rnn_sizes, num_layer)
+inputs = {input, prev_h1, prev_h2, prev_h3}
 outputs = rnnmodule3:forward(inputs)
 print('general multiple outputs:', outputs)
 outputs = rnnmodule4:forward(inputs)
